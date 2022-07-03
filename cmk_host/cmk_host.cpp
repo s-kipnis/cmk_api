73f6676ba5d59601fb2a7d911a151ae7c292bfcd
Sergey Kipnis <sergey.kipnis@tribe29.com>
2022-07-03 18:19:25 +0200

    Add test app in C++ cmk_host

---
+++ cmk_host/cmk_host.cpp
@@ -0,0 +1,35 @@
+﻿// cmk_host.cpp : Defines the entry point for the application.
+//
+
+#include "cmk_host.h"
+
+#include <windows.h>
+
+#include <cstdint>
+#include <filesystem>
+#include <string_view>
+
+using CommandFunc = int(__stdcall *)(const char *text, uint32_t);
+
+int main() {
+    // Load
+    constexpr std::string_view x =
+        R"(C:\Users\sk\git\cmk_api\target\debug\cmk_api.dll)";
+    HINSTANCE dll = ::LoadLibrary(x.data());
+
+    if (dll == nullptr) {
+        std::cout << "could not load the dynamic library" << std::endl;
+        return 1;
+    }
+
+    // resolve function address here
+    auto command =
+        reinterpret_cast<CommandFunc>(::GetProcAddress(dll, "command"));
+    if (command == nullptr) {
+        std::cout << "could not locate the function" << std::endl;
+        return 1;
+    }
+    command("abcde", 5);
+    std::cout << "Hello CMake." << std::endl;
+    return 0;
+}
